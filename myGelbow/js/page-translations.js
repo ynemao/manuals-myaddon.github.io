7ce901978e8f5ab52c31038a2ad87aeb1cf00114
ynemao <121027474+ynemao@users.noreply.github.com>
2022-12-22 17:23:41 +0100

    Update

---
+++ myGelbow/js/page-translations.js
@@ -0,0 +1,46 @@
+var linguJSON = {
+    "languages": [
+            {
+            "lang_name": "Nederlands",
+            "lang_code": "nl",
+            "url_pattern": "?"
+        },
+        {
+            "lang_name": "English",
+            "lang_code": "en",
+            "url_pattern": "?"
+        },
+        {
+            "lang_name": "Français",
+            "lang_code": "fr",
+            "url_pattern": "?"
+        },
+        {
+            "lang_name": "Deutsch",
+            "lang_code": "de",
+            "url_pattern": "?"
+        }
+    ],
+    "translated_segments": [
+        {
+            "source": "Ready to go global?",
+            "target_en": "¿Está listo para el mundo?",
+            "target_fr": "",
+            "target_de": ""
+        },
+        {
+            "source": "get found by search engines and visitors in other languages",
+            "target_en": "¿Está listo para el mundo?",
+            "target_fr": "",
+            "target_de": ""
+        },
+        {
+            "source": "Powered by <strong>Lingumania</strong>",
+            "target_en": "¿Está listo para el mundo?",
+            "target_fr": "",
+            "target_de": ""
+        }
+    ]
+};
+                                        
+                                        
